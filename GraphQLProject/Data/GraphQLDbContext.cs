9772225f5029edbc5bd051b31ebf86f3a7e487f9
shabioks <monicaab.mtz95@gmail.com>
2024-10-13 00:45:18 -0600

    feature: Adding Migrations of EF Db

--- GraphQLProject/Data/GraphQLDbContext.cs
+++ GraphQLProject/Data/GraphQLDbContext.cs
@@ -11,6 +11,18 @@ namespace GraphQLProject.Data
 		}
 
 		public DbSet<Menu> Menus { get; set; }
-	}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Menu>().HasData(
+                new Menu() { Id = 1, Name = "Burger", Description = "A juicy chicken burger with lettuce and cheese", Price = 10.50 },
+                new Menu() { Id = 2, Name = "Salad", Description = "Tomato and fresh lettuce", Price = 10.50 },
+                new Menu() { Id = 3, Name = "Burrito", Description = "Tortilla ", Price = 10.50 },
+                new Menu() { Id = 4, Name = "Pasta Alfredo", Description = "Creamy Alfredo sauce with fettuccine pasta", Price = 10.50 },
+                new Menu() { Id = 5, Name = "Chocolate", Description = "Warm chocolate brownie with ice cream" , Price = 10.50 }
+            );
+        }
+    }
 }
 
---
+++ GraphQLProject/Migrations/20241012065238_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQLProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
